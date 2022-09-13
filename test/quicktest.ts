e08bd023caad56028c503db77e51335ed720f37f
william_doyle <mcbidebilly@gmail.com>
2022-09-13 16:41:37 -0400

    tidy up

--- test/quicktest.ts
+++
@@ -1,7 +0,0 @@
-import { randomBytes } from 'crypto'
-import { ethers } from 'ethers';
-const hash = (input: string) => ethers.utils.keccak256(ethers.utils.toUtf8Bytes(input))
-const hash_b = (input: string) => ethers.utils.keccak256(input)
-
-console.log(randomBytes(32).toString('hex'))
-console.log(hash(randomBytes(32).toString('hex')).substring(2))
